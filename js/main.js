dfa682bbb46b0f28e024bdc7c961225282ae3f1d
Ridoncules <hgsteggles@gmail.com>
2016-12-11 18:20:46 +0000

    Google map fix; portfolio item layout fix; added posts category for portfolio items; changed menu item texts.

--- js/main.js
+++ js/main.js
@@ -14,10 +14,15 @@ $(document).ready(function(){
 
 
 	$("#clients-logo").owlCarousel({
+
 		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
+		items : 4,
+		margin: 10,
+		responsive:{
+			600:{
+				items:4
+			}
+		}
 	});
 
 	$("#works-logo").owlCarousel({
@@ -26,17 +31,6 @@ $(document).ready(function(){
 		itemsDesktop : [1199,5],
 		itemsDesktopSmall : [979,5],
 	});
-
-
-	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
-		}
-
 
 	// Counter
 
